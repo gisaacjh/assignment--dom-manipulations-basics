1733e6d6b6664b409a2bb309f3ba1b03141cde03
Isaac <gisaacjh@outlook.com>
2017-10-10 09:30:38 -0500

    upload1

--- README.md
+++ README.md
@@ -3,17 +3,22 @@
 ##Setup
 1 -- navigate to your `assignments` directory:
 ```
-cd ~/Muktek/assignments
+cd ~/TIY/assignments
 ```
 
-2 -- Create the project folder
+2 -- Create the project folder for the correct number
 ```
 mkdir assignment-XX
 cd assignment-XX
 ```
 
-3 -- Download the `.zip` file, move it to your `assignment-XX` folder, and unzip it:
+3 -- Inside your `assignment-XX` folder, download + unzip the file:
+```
+curl https://raw.githubusercontent.com/TIY-Charleston-Front-End-Engineering/Course-Guide/master/assignments/dom-manipulations-basics/ssignment-dom-manipulations-basics.zip > assignment-dom-manipulations-basics.zip
 
-https://github.com/muktek/assignment--dom-manipulations-basics/blob/master/assignment-dom-manipulations-basics.zip
+unzip assignment-dom-manipulations-basics.zip
+```
 
 4 -- Complete the tasks outlined in the assignment. All of your work will be in the `main.js` file.
+
+5 -- Submit in Newline
--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 
 document.querySelector("#inject-html button").addEventListener('click',function(){
   // TASK #1
+  //  Cuando el evento "click" ocurra en el elemento"Push Me" <button>
+  //  Insertar un texto con un elemento <h2> dentro de un <div class="answer-box">
+
   var answerBoxEl = document.querySelector('#inject-html .answer-box')
-  answerBoxEl.innerHTML = "<h2>Here is some text for you!!</h2>"
+  answerBoxEl.innerHTML = "<h2>Work done!!</h2>"
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
   // TASK #2
+  // Cada evento 'click' debe doblar el valor del número debajo
+  // hacer que el evento 'click' se multiplique por 2
   var answerPTagWithValue = document.querySelector('#compoundInvestment')
   // you do the rest
+    answerPTagWithValue.innerHTML = answerPTagWithValue.innerHTML * 2
 
 })
 
-
+var contador = 0
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
-})
+  // cambiar el color del circulo de #FFFFFF a #000000
+  // el interruptor es un click
+  var answerColorBox = document.querySelector('#circle-bw')
+  contador ++
+  if (answerColorBox.innerHTML % 2 === 0) {
+      answerColorBox.style.background = 'black'
+  } else {
+      answerColorBox.style.background = 'white'
+}
 
+})
+var contador = 0
 document.querySelector("#blow-up button").addEventListener('click',function(){
   // TASK #4}
+  //manipular el div para que el circulo duplique su altura y anchura
+  //el circulo debe ser mayor o igual a 320px
+  // en ese momento debe retornara su tamaño original 40px x 40px
+  var answerSize = document.querySelector('circle-red')
+  contador ++
+  answerSize.style.
+  console.log('circle-red');
 })
 
 document.querySelector("#remove button").addEventListener('click',function(){
